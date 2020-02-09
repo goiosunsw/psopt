5cbce9e6b21edb5ccc61de2e947395947ecfb71c
Andre Almeida <a.almeida@unsw.edu.au>
2020-02-10 08:04:17 +1100

    spline constraint and randomization

--- psopt/spline_fitter.py
+++ psopt/spline_fitter.py
@@ -1,7 +1,7 @@
+import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
 from scipy.optimize import LinearConstraint
 from psopt import FunctionOptimizer
-import numpy as np
 
 
 def nuarmax(x,y,rad=1.0):
@@ -16,9 +16,11 @@ def nuarmax(x,y,rad=1.0):
     
 
 class SplineOptimizer(FunctionOptimizer):
-    def __init__(self,degree=3,n_nodes=3,
+    def __init__(self,degree=3,n_nodes=5,
                  x=None,y=None,
                  xc=None,yc=None,
+                 xclim=None,
+                 yclim=None,
                  freeze_edges=True,**kwargs):
         self.degree = degree
         self.n_nodes=n_nodes
@@ -26,23 +28,56 @@ class SplineOptimizer(FunctionOptimizer):
         FunctionOptimizer.__init__(self, **kwargs)
 
         self.min_dx = 0.001
+        self.xc = xc
+        self.yc = yc
         if x is not None and y is not None:
             self.set_data(x,y)
+        self._take_step = self._take_step_spline
+        self.set_lims(xclim=xclim, yclim=yclim)
+        self._register_free_controls()
+
+    def set_lims(self,xclim=None,yclim=None):
+        if xclim is None:
+            try:
+                self.xmin
+            except AttributeError:
+                self.xmin = np.min(self.xm)
+                self.xmax = np.max(self.xm)
+        else:
+            self.xmin = np.min(xclim)
+            self.xmax = np.max(xclim)
+        if yclim is None:
+            try:
+                self.ymin
+            except AttributeError:
+                self.ymin = np.min(self.ym)
+                self.ymax = np.max(self.ym)
+        else:
+            self.ymin = np.min(yclim)
+            self.ymax = np.max(yclim)
             
-        if xc is None:
+    def init_control_points(self):
+        if self.xc is None:
             self.xc,yg=self.guess_nodes()
         else:
-            self.xc=xc
-            yg = np.interp(xc,x,y)
-        if yc is None:
+            yg = np.interp(self.xc,self.xm,self.ym)
+        if self.yc is None:
             self.yc=yg
             
         self.set_control_points(self.xc,self.yc)
         
-        self.set_objective_function(self.fpred,params=self.param_tuplist)
         
         #print(super())
-        
+
+    def _register_free_controls(self):
+        self.free_x_idx = []
+        self.free_y_idx = []
+        for ii, parname in enumerate(self.free_params):
+            if parname[0]=='x':
+                self.free_x_idx.append(ii)
+            if parname[0]=='y':
+                self.free_y_idx.append(ii)
+
     @property
     def param_tuplist(self):
         return [(k,v) for k,v in self.param_dict.items()]
@@ -59,26 +94,45 @@ class SplineOptimizer(FunctionOptimizer):
         xmin=min(self.xm)
         xmax = max(self.xm)
         for xi,xx in enumerate(xc):
-            self.set_param('x{}'.format(xi),xx,bounds=(xmin,xmax))
+            parname = 'x{}'.format(xi)
+            try:
+                bounds = self.bounds[parname]
+            except KeyError:
+                self.set_param(parname,xx,bounds=(xmin,xmax))
             #self.set_bounds('x{}'.format(xi),[xmin,xmax])
         if self.freeze_edges:
             self.fitting_off('x0')
             self.fitting_off('x{}'.format(xi))
         for yi,yy in enumerate(yc):
             self.set_param('y{}'.format(yi),yy)
-            
-        if self.freeze_edges:
-            nconst = len(xc)-3
-        else:
-            nconst = len(xc)-1
-        self.constraint_mx = np.zeros((nconst,nconst+1+len(yc)))
-        self.constraint_lb = np.ones(nconst)*self.min_dx
-        for ii in range(nconst):
-            self.constraint_mx[ii,ii]=-1
-            self.constraint_mx[ii,ii+1]=1
-        self.constraints = LinearConstraint(self.constraint_mx,self.constraint_lb,np.inf)
-    
         self._control_xy_to_spl(xc,yc)
+        self.set_constraints()
+
+    def set_constraints(self):
+        n_free_x = sum([1 for x in self.free_params if x[0]=='x'])
+        if n_free_x>0:
+            nconst = n_free_x - 1
+            self.constraint_mx = np.zeros((nconst,len(self.free_params)))
+            self.constraint_lb = np.ones(nconst)*self.min_dx
+            for ii in range(nconst):
+                self.constraint_mx[ii,ii]=-1
+                self.constraint_mx[ii,ii+1]=1
+            self.constraints = LinearConstraint(self.constraint_mx,self.constraint_lb,np.inf,keep_feasible=True)
+        else:
+            self.constraints = None
+    
+        
+    def _take_step_spline(self, args):
+        min_dx = 0.001
+        newx = np.random.uniform(self.xmin,self.xmax,len(self.free_x_idx))
+        newx = np.sort(newx)
+        dx = np.diff(newx)
+        dx[dx<min_dx]=min_dx
+        newx = np.cumsum(np.insert(dx,0,newx[0]))
+        stepx = newx - args[self.free_x_idx]
+        newy = np.random.uniform(self.ymin,self.ymax,len(self.free_y_idx))
+        return np.concatenate((newx, newy))
+
         
     def _control_xy_to_spl(self, xc, yc):
         idx = np.argsort(xc)
@@ -111,23 +165,108 @@ class SplineOptimizer(FunctionOptimizer):
             yc.append(pp)
             yi+=1
         self._control_xy_to_spl(xc,yc)
+
+    def obj_fun_wrap(self, x, func, *extra):
+        return func(x)
             
-    def obj_fun_wrap(self, x, geom_func):
-        return geom_func(x)
-    
     def fpred(self,x,*parlist):
         self._param_list_to_spl(parlist)
         y = self.obj_fun_wrap(x, self.spl)
         return(y)
-            
+
+    def _param_dict_to_spl(self,pardict):
+        ii=0
+        xc=[]
+        yc=[]
+        while True:
+            try:
+                xc.append(pardict['x{}'.format(ii)])
+                yc.append(pardict['y{}'.format(ii)])
+                ii+=1
+            except KeyError:
+                break
+        self._control_xy_to_spl(xc,yc)
+                
     def fit_spline(self):
-        self.optimize(self.ym,self.xm)#,constraints=self.constraints)
+        ret = self.optimize(self.ym,self.xm)#,constraints=self.constraints)
+        self._param_dict_to_spl(ret)
+        return ret
+
+    def random_x_controls_inside_data(self):
+        """
+        returns a random set of controls
+        and boundaries
+        """
+        xmin = np.min(self.xm)
+        xmax = np.max(self.xm)
+        newxc = np.random.uniform(xmin,xmax,len(self.xc)-2)
+        newxc = np.sort(newxc)
+        newxc = np.concatenate(([xmin], newxc, [xmax]))
+        return newxc, [(xmin, xmax)] * len(newxc)
+
+    def random_x_controls_around_data(self, range_mult=1):
+        """
+        returns a random set of controls
+        and boundaries
+        """
+
+        dx = np.max(self.xm)-np.min(self.xm)
+        xmin = np.min(self.xm) - dx*range_mult
+        xmax = np.max(self.xm) + dx*range_mult
+        newxc = np.random.uniform(xmin,xmax,len(self.xc))
+        newxc = np.sort(newxc)
+        return newxc, [(-np.inf, np.inf)] * len(newxc)
+
+    def run_multi_fit(self, iterations=10):
+        xc_st = []
+        xc_fit = []
+        yc_fit = []
+        xc_cost = []
+        xc_ret = []
+        xc_par = []
+
+        for ii in range(iterations):
+            if self.freeze_edges:
+                newxc, bounds = self.random_x_controls_inside_data()
+            else:
+                newxc, bounds = self.random_x_controls_around_data()
+            self.xc = newxc
+            bound_dict = {'x%d'%ii:v for ii,v in enumerate(bounds)}
+            self.set_bound_dict(bound_dict)
+            xcst = self.xc
+            self.yc = None
+            try:
+                self.init_control_points()
+            except  ValueError:
+                continue
+            xc_st.append(xcst)
+            try:
+                par=self.fit_spline()
+                #ax = so.plot(ax)
+                xc_cost.append(self._ret['fun'])
+                xc_ret.append(self._ret)
+            except ValueError:
+                xc_cost.append(np.inf)
+                xc_ret.append(traceback.format_exc())
+                par=[]
+            xc_fit.append(self.xc.copy())
+            yc_fit.append(self.yc.copy())
+            xc_par.append(par)
+
+        xc_cost = np.array(xc_cost)
+        xc_fit = np.array(xc_fit)
+        yc_fit = np.array(yc_fit)
+
+        return xc_cost, xc_fit, yc_fit
+            
         
     def set_data(self,x,y):
         self.xm = np.asarray(x)
         self.ym = np.asarray(y)
         self.min_dx = (max(self.xm)-min(self.xm))*1e-4
-        
+        self.init_control_points()
+        self.set_objective_function(self.fpred,params=self.param_tuplist)
+      
     @property
     def knots(self):
         return self.spl.get_knots()
